2527002262b68c192e99e80ec4c58c12aff3cbdc
julesZIV <jules.zivkovic@efrei.net>
2023-11-30 17:13:15 +0100

    fonction eco clim

---
+++ chatbot_1er_rendu/test.py
@@ -0,0 +1,19 @@
+def écologie(files_names):
+    '''
+    Trouve le mot ecologie dans texte
+    '''
+    L=['écologie','climat']
+    cont=9999999999999
+    nom_pres=''
+    for e in files_names:
+        f = open(entry_dir + '/' + e, 'r')
+        contenu=f.read()
+        f.close()
+        contenu=contenu.split()
+        for i in range(len(contenu)):
+            if contenu[i] in L:
+                if i < cont:
+                    nom_pres=e
+                    cont=i
+    if cont<9999999999999:
+        return nom_pres, cont
